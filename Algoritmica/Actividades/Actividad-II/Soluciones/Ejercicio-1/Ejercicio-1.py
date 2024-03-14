f64af0b7a3d447892640ef14bc5a11a29976a2fa
Jorgego30 <jorgegomgar5@gmail.com>
2024-03-14 09:45:16 +0100

    Se corrige la notacion prefija y se añaden dos archivos necesarios para la resolucion del ejercicio

--- Algoritmica/Actividades/Actividad-II/Soluciones/Ejercicio-1/Ejercicio-1.py
+++ Algoritmica/Actividades/Actividad-II/Soluciones/Ejercicio-1/Ejercicio-1.py
@@ -1,8 +1,8 @@
 """
 ---------------------|-------------------------|
 Expresiones infijas  |   Expresiones prefijas  |
-(A+B)*(C+D)*(E+F)    |   (AB+)(CD+)*(EF+)*     |
-A+((B+C)*(D+E))      |   A((BC+)(DE+)*)+       |
-A*B*C*D+E+F          |   AB*CD*EF++            |
+(A+B)*(C+D)*(E+F)    |   *(+AB)*(+CD)(+EF)*     |
+A+((B+C)*(D+E))      |   +A(*(+BC)(+DE))       |
+A*B*C*D+E+F          |   *AB*CD++E+F            |
 ---------------------|-------------------------|
 """
