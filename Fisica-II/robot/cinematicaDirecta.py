de66e3cd1d9ae8c52d368e9321ef4f9cfe1b3ff8
Jorgego30 <jorgegomgar5@gmail.com>
2024-04-23 10:04:54 +0200

    Se importan bien los datos del archivo yaml

--- Fisica-II/robot/cinematicaDirecta.py
+++ Fisica-II/robot/cinematicaDirecta.py
@@ -6,16 +6,27 @@ with open('robot.yaml', 'r') as file:
 robotname = robotdata['robot']['name']
 eslabones = robotdata['robot']['links']
 
-joint_coords = np.array([0,0,0])
+joint_coords = []
+joint_axis = []
+length = []
 
 for eslabon in eslabones:
 	print("eslabón ID = ", eslabon['id'])
 	print("tipo de eslabón = ", eslabon['type'])
 	print("coordenadas articulación = ", eslabon['joint_coords'])
-	a = np.array([eslabon['joint_coords']])
-	np.vstack((joint_coords,a))
+	joint_coords.append(eslabon['joint_coords'])
 	print("Eje rotación (R) o expansión (P) = ", eslabon['joint_axis'])
+	joint_axis.append(eslabon['joint_axis'])
+	print("longitud articulación = ", eslabon['length'])
+	length.append(eslabon['length'])
 
 
 
 print(joint_coords)
+print(joint_axis)
+print(length)
+
+# Definimos los vectores que van de cada eje al siguiente
+q = []
+for i in length:
+	q[i] = 
